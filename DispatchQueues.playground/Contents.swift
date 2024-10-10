f0a2a7e0349199a8677154c094bf2c80f128f7a8
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-10 16:08:06 +0200

    GCD Examples and readme file update

---
+++ DispatchQueues.playground/Contents.swift
@@ -0,0 +1,299 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+//serial queues - default if no attributes are specified - the output will be printer in order the code is executed
+
+let serialQueue = DispatchQueue(label: "serialQueue")
+
+serialQueue.async {
+    print("Task 1 started")
+        // Do some work..
+    print("Task 1 finished")
+}
+
+serialQueue.async {
+    print("Task 2 started")
+        // Do some work..
+    print("Task 2 finished")
+}
+
+
+//concurrent queues - the output might not be in order
+
+let myQueue = DispatchQueue(label: "com.multithreading.concurr", attributes: .concurrent)
+myQueue.async {
+    for i in 0..<10 {
+        print("1")
+    }
+}
+myQueue.async {
+    for j in 0..<10 {
+        print("2")
+    }
+}
+
+
+//QOS: this will help to set the priority to the queues. Global queues does not run on main thread but it will run on any available thread
+//Global Queues
+// Fourth loop (User-interactive) always ends first. The third loop (User-initiated) follows it, then the second loop (Utility) ends. At last, you can see the first loop (Background) ends the task execution.
+DispatchQueue.global(qos: .background).async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    
+    print("---- First loop start ----- qos: \(qos)")
+    for i in 0...5 {
+        print("Value is \(i)")
+    }
+    print("---- First loop end -----")
+}
+
+DispatchQueue.global(qos: .utility).async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    
+    print("---- Second loop start ----- qos: \(qos)")
+    for i in 6...10 {
+        print("Value is \(i)")
+    }
+    print("---- Second loop end -----")
+}
+
+DispatchQueue.global(qos: .userInitiated).async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    
+    print("---- Third loop start ----- qos: \(qos)")
+    for i in 11...15 {
+        print("Value is \(i)")
+    }
+    print("---- Third loop end -----")
+}
+
+DispatchQueue.global(qos: .userInteractive).async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    
+    print("---- Fourth loop start ----- qos: \(qos)")
+    for i in 16...20 {
+        print("Value is \(i)")
+    }
+    print("---- Fourth loop end -----")
+}
+
+
+//Custom Queues - e can create custom queues for specific use cases to have more control.
+// Between 11–20 is run on the concurrent queue and they are on different blocks, so the outputs are unordered. Between 0–10 is run on the serial queue and the outputs are ordered. Also, user-interactive is higher level than the utility so we can see the 20 before the 10.
+
+// Serial Queue
+let queueA = DispatchQueue(label: "QueueA",
+                           qos: .utility)
+
+// Concurrent Queue
+let queueB = DispatchQueue(label: "QueueB",
+                           qos: .userInteractive,
+                           attributes: .concurrent)
+
+queueA.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- First loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 0...5 {
+        print("Value is \(i)")
+    }
+    print("---- First loop end -----")
+}
+
+queueA.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Second loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 6...10 {
+        print("Value is \(i)")
+    }
+    print("---- Second loop end -----")
+}
+
+queueB.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Third loop start ----- qos:\(qos), queueName:\(name)")
+    for i in 11...15 {
+        print("Value is \(i)")
+    }
+    print("---- Third loop end -----")
+}
+
+queueB.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Fourth loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 16...20 {
+        print("Value is \(i)")
+    }
+    print("---- Fourth loop end -----")
+}
+
+
+//Setting the target queue
+//Even the queueD is concurrent when it is targetted to queueC it executed its task in a serial manner. So all the tasks you submitted to queueB will be executed serially because of the target queue. You may realize that order of execution is changed but the QoS of the queueB is the same.
+// Serial Queue
+let queueC = DispatchQueue(label: "QueueC",
+                           qos: .utility)
+
+// Concurrent Queue
+let queueD = DispatchQueue(label: "QueueD",
+                           qos: .userInteractive,
+                           attributes: .concurrent,
+                           target: queueC) //******Changed Line******
+
+
+queueC.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- First loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 0...5 {
+        print("Value is \(i)")
+    }
+    print("---- First loop end -----")
+}
+
+queueC.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Second loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 6...10 {
+        print("Value is \(i)")
+    }
+    print("---- Second loop end -----")
+}
+
+
+queueD.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Third loop start ----- qos:\(qos), queueName:\(name)")
+    for i in 11...15 {
+        print("Value is \(i)")
+    }
+    print("---- Third loop end -----")
+}
+
+
+queueD.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Fourth loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 16...20 {
+        print("Value is \(i)")
+    }
+    print("---- Fourth loop end -----")
+}
+
+
+//The QoS value of queueB is utility even if we set it to the background. But this wasn’t the case when the QoS of queueB was user-interactive. GCD won’t change QoS if the target queue is lower than the existing queue. It only changes the QoS if the target queue’s QoS value is in higher order than the current queue.
+
+// Serial Queue
+let queueE = DispatchQueue(label: "QueueE",
+                           qos: .utility)
+
+// Concurrent Queue
+let queueF = DispatchQueue(label: "QueueF",
+                           qos: .background,
+                           attributes: .concurrent,
+                           target: queueE)
+
+
+queueE.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- First loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 0...5 {
+        print("Value is \(i)")
+    }
+    print("---- First loop end -----")
+}
+
+queueE.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Second loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 6...10 {
+        print("Value is \(i)")
+    }
+    print("---- Second loop end -----")
+}
+
+
+queueF.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Third loop start ----- qos:\(qos), queueName:\(name)")
+    for i in 11...15 {
+        print("Value is \(i)")
+    }
+    print("---- Third loop end -----")
+}
+
+
+queueF.async {
+    let qos = DispatchQoS.QoSClass(rawValue: qos_class_self()) ?? .unspecified
+    let name =  String(cString:__dispatch_queue_get_label(nil))
+    print("---- Fourth loop start ----- qos: \(qos), queueName:\(name)")
+    for i in 16...20 {
+        print("Value is \(i)")
+    }
+    print("---- Fourth loop end -----")
+}
+
+
+
+//Dispatch Groups
+
+//A dispatch group is a group of tasks that is monitored as one unit. This way, collections of tasks can be aggregated. Multiple work items are attached to a group and scheduled for asynchronous execution. This way, multiple processes can be started but only one event is needed which occurs when all tasks have been completed
+func myTask1(dispatchGroup:DispatchGroup){
+    DispatchQueue.global().async {
+        print("Task 1 finished")
+        dispatchGroup.leave()
+    }
+}
+
+func myTask2(dispatchGroup:DispatchGroup){
+    DispatchQueue.global().async {
+        print("Task 2 finished")
+        dispatchGroup.leave()
+    }
+}
+ 
+func myDispatchGroup(){
+    let dispatchGroup = DispatchGroup()
+    dispatchGroup.enter()
+    myTask1(dispatchGroup: dispatchGroup)
+    dispatchGroup.enter()
+    myTask2(dispatchGroup: dispatchGroup)
+ 
+    dispatchGroup.notify(queue: .main) {
+        print("All tasks finished.")
+    }
+}
+
+
+//Dispatch Work Item
+//A dispatch work item encapsulates a block of code and provides the flexibility to cancel the given task.
+//
+//Imagine that you are using the search function in an app. With every typed letter, a new search call is made and the previous one is canceled.
+   
+let workItem = DispatchWorkItem {
+ print("Work Item executing")
+}
+let queue = DispatchQueue(label: "com.example")
+queue.async(execute: workItem)
+
+workItem.cancel()
+if workItem.isCancelled {
+  print("Work item has been canceled")
+}
+
+//Async after
+//This method can be utilized to delay the execution of a task. GCD enables the developer to set the amount of time after which the task should be run.
+let time = 2.0
+DispatchQueue.main.asyncAfter(deadline: .now() + time){
+    print("Delayed task")
+}
--- README.md
+++ README.md
@@ -1,1 +1,72 @@
 # GCD
+
+1. Main Queue
+The main queue is a system-created serial queue that uses the main thread. If we want to update UI, we have to use the main queue because UI is tied to the main thread.
+If you don’t create any queue at all in XCode you are using the main queue in a synchronous manner.
+Features:
+*         System Created
+*         Serial
+*         Uses main thread
+*         UI tied to the main queue
+2. Global Concurrent Queue
+Global concurrent queues are system-created concurrent queues. These types of queues can not use the main thread.
+Features:
+*         System Created
+*         Concurrent
+*         Can’t use the main thread.
+Global concurrent queues can serve most of the concurrent execution needs we have.
+
+
+
+Quality Of Service (QOS) - sets priority
+A quality-of-service (QOS) class categorizes the work items which are performed on a dispatch queue. With this parameter, the priority of a given task is specified. There are four QOS classes that may be assigned to a queue.
+Background
+These are tasks that can take minutes or hours to complete such as loading or processing large amounts of data. They are not time-critical and the user needs to be able to do other things while they are happening. Such tasks are mapped to a low-priority queue.
+Utility
+These tasks take seconds or minutes and should be completed immediately. An example of a utility task would be one with a loading bar, such as a download.
+User-initiated
+These are tasks initiated by the user which should be executed right away, such as opening a document. They will be mapped to a high-priority queue and should only take a few seconds or less.
+User-interactive
+These are UI tasks that need to be finished immediately to ensure that the user is able to continue with the next interaction. They are mapped to the queue with the highest priority and run on the main thread.
+
+
+Order and manner of dispatch
+When using queues, the order and manner in which tasks are dispatched need to be chosen. GCD queues can be serial or concurrent and pushing tasks to them can happen synchronously or asynchronously.
+
+Synchronous vs asynchronous
+When a work item is scheduled synchronously, the execution of code that is happening on the current thread is paused until the item’s execution is completed. It is important not to execute a work item synchronously on the main queue as this results in a deadlock.
+
+When it is scheduled asynchronously, the code on the current thread continues to be executed while the work item is scheduled to run on another thread.
+
+Serial vs concurrent
+In a serial queue, tasks are executed one after the other according to the FIFO algorithm. The serial queue just needs one thread since only one task is running at a time.
+A concurrent queue creates as many threads as it has tasks dispatched into it. The developer does not control the creation of the threads.
+
+
+Potential Problems
+
+When multiple threads are running concurrently on shared memory, they must be controlled and used in the most advantageous way. Determining how to manage them efficiently can be difficult.
+
+It is importantt to not keep adding more and more new threads to a program. More threads can mean more problems and the way they work together must be designed very carefully.
+If threads are not managed correctly, there are several problems that can occur.
+Deadlock
+
+Deadlocks happen when two or more threads are unable to continue since they require a resource that is held by another thread. That thread requires a resource that is held by one of the threads waiting for it to be completed. In essence, each thread is waiting for another one to finish, halting any progress.
+Race conditions
+
+Race conditions occur when threads execute a section of code that can be managed concurrently by several threads, making them “race” to alter shared resources. This causes data to be inconsistent as the program output changes depending on which thread wins the race.
+Starvation
+
+Starvation happens when a thread never gets any CPU time or access to shared resources as other processes are given a higher priority. The starved thread remains at the end of the scheduling queue and is never executed.
+Livelock
+
+A livelock occurs when two threads are executing actions in response to one another instead of continuing with their task. Think of it like two people trying to pass each other in a narrow hallway and both moving in the same direction, blocking each other. Consequently, they both move to the other side, blocking each other again and so on.
+
+
+Multithreading on multiple cores
+
+Since the introduction of machines with multiple cores, it has been possible to have a dedicated processor run each thread, enabling parallelism.
+
+With a single core, the application would need to switch back and forth to create the illusion of multitasking. With multiple cores, the underlying hardware can be used to run each thread on a dedicated core.
+
+This allows the application to take full advantage of the available processing power, making the program more efficient and minimizing the risk of problems associated with concurrency as not all threads are running on the same core.
