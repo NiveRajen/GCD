6f02fe6b2bf8d78246b81ccc001a7b93492db089
Nivedha Rajendran <nivedha.rajen@gmail.com>
2024-10-15 14:44:02 +0200

    Dispatch Semaphores

--- DispatchQueues.playground/Contents.swift
+++ DispatchQueues.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Dispatch
 
 var greeting = "Hello, playground"
 
@@ -297,3 +298,44 @@ let time = 2.0
 DispatchQueue.main.asyncAfter(deadline: .now() + time){
     print("Delayed task")
 }
+
+
+
+// Create a semaphore with an initial value of 1
+//let semaphore = DispatchSemaphore(value: 1)
+//
+//// Shared resource
+//var resourceCounter = 0
+//
+//// Concurrent tasks
+//func taskOne() {
+//    semaphore.wait() // Acquire the semaphore permit
+//    for _ in 1...5 {
+//        print(Thread.current)
+//        resourceCounter += 1
+//        print("Task One: \(resourceCounter)")
+//    }
+//    semaphore.signal() // Release the semaphore permit
+//}
+//
+//func taskTwo() {
+//    semaphore.wait() // Acquire the semaphore permit
+//    for _ in 1...5 {
+//        print(Thread.current)
+//        resourceCounter -= 1
+//        print("Task Two: \(resourceCounter)")
+//    }
+//    semaphore.signal() // Release the semaphore permit
+//}
+//
+//// Create a concurrent queue
+//let concurrentQueue = DispatchQueue(label: "com.exp.conQueue", attributes: .concurrent)
+//
+//// Run the tasks concurrently
+//concurrentQueue.async {
+//    taskOne()
+//}
+//
+//concurrentQueue.async {
+//    taskTwo()
+//}
